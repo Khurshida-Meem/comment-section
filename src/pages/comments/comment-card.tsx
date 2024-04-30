f52d258ab78dd5526918cf7641c5480c3c20ebf4
Khurshida Meem <meem@ibos.io>
2024-04-30 17:38:38 +0600

    updated layout

--- src/pages/comments/comment-card.tsx
+++ src/pages/comments/comment-card.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Avatar, Grid } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 
@@ -10,17 +10,27 @@ const CommentCard = () => {
           <Grid item md={1}>
             <div style={{ maxWidth: "40px" }} className="light_bg br4 pv2">
               <div>
-                <AddIcon className="pointer" sx={{ fontSize: "14px" }} />
+                <AddIcon
+                  className="pointer"
+                  sx={{ fontSize: "14px", color: "darkgray" }}
+                />
               </div>
-              <div className="mv2">1</div>
+              <div className="mv2 b blue">1</div>
               <div>
-                <RemoveIcon className="pointer" sx={{ fontSize: "14px" }} />
+                <RemoveIcon
+                  className="pointer"
+                  sx={{ fontSize: "14px", color: "darkgray" }}
+                />
               </div>
             </div>
           </Grid>
           <Grid item md={11}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            pariatur nemo corporis! Consectetur modi quia cupiditate ut unde vel
+            <Avatar
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+              sx={{ width: 24, height: 24 }}
+            />
+            <p>ghjkl</p>
           </Grid>
         </Grid>
       </div>

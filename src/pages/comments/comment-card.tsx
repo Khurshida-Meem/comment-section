1f5bd303af30387b41235cfd4aa51101ada92b6d
Khurshida Meem <meem@ibos.io>
2024-04-30 16:57:09 +0600

    updated like dislike

--- src/pages/comments/comment-card.tsx
+++ src/pages/comments/comment-card.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 const CommentCard = () => {
   return (
-    <div className="flex justify-center">
-      <div></div>
-    </div>
+    <center className="mt5">
+      <div className="w-50 bg-white pa3 br3">
+        <Grid container>
+          <Grid item md={1}>
+            <div style={{ maxWidth: "40px" }} className="light_bg br4 pv2">
+              <div>
+                <AddIcon className="pointer" sx={{ fontSize: "14px" }} />
+              </div>
+              <div className="mv2">1</div>
+              <div>
+                <RemoveIcon className="pointer" sx={{ fontSize: "14px" }} />
+              </div>
+            </div>
+          </Grid>
+          <Grid item md={11}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            pariatur nemo corporis! Consectetur modi quia cupiditate ut unde vel
+          </Grid>
+        </Grid>
+      </div>
+    </center>
   );
 };
 

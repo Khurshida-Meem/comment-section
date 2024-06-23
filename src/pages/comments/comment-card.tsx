0be638b3b69aa97d861fb24f2f36d27f4ea911dd
Khurshida Meem <meem@ibos.io>
2024-06-23 17:23:46 +0600

    added external data and images

--- src/pages/comments/comment-card.tsx
+++ src/pages/comments/comment-card.tsx
@@ -4,11 +4,11 @@ import RemoveIcon from "@mui/icons-material/Remove";
 
 const CommentCard = () => {
   return (
-    <center className="mt5">
+    <center className="mt5 ">
       <div className="w-50 bg-white pa3 br3">
         <Grid container>
           <Grid item md={1}>
-            <div style={{ maxWidth: "40px" }} className="light_bg br4 pv2">
+            <div style={{ maxWidth: "40px" }} className="light_bg br4 pv2 tc">
               <div>
                 <AddIcon
                   className="pointer"
@@ -24,13 +24,15 @@ const CommentCard = () => {
               </div>
             </div>
           </Grid>
-          <Grid item md={11}>
-            <Avatar
-              alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
-              sx={{ width: 24, height: 24 }}
-            />
-            <p>ghjkl</p>
+          <Grid item md={11} sx={{ textAlign: "initial" }}>
+            <div>
+              <Avatar
+                alt="Remy Sharp"
+                src="/static/images/avatar/1.jpg"
+                sx={{ width: 24, height: 24 }}
+              />
+              <p>ghjkl</p>
+            </div>
           </Grid>
         </Grid>
       </div>
